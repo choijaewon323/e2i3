4a836c1ecc9dc817e17adf5d19865deee2b8605a
leesewon00 <dltpdnjs2000@gmail.com>
2023-03-29 17:42:50 +0900

    s1

---
+++ src/main/java/com/example/e2i3/service/MemberService.java
@@ -0,0 +1,44 @@
+package com.example.e2i3.service;
+
+import com.example.e2i3.dto.MemberDTO;
+import com.example.e2i3.entity.Member;
+import com.example.e2i3.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 삭제
+    @Transactional
+    public Integer deleteById(MemberDTO memberDTO) {
+        Optional<Member> byMemberEmail = memberRepository.findByEmail(memberDTO.getEmail());
+        if(byMemberEmail.isPresent()){
+            //성공
+            //비밀번호 확인 조건 설정
+            Member memberEntity = byMemberEmail.get();
+            Long id = memberEntity.getId();
+
+            String memberPassword = memberEntity.getPassword();
+            String memberPassword1 = memberDTO.getPassword();
+            if(memberPassword.equals(memberPassword1)){
+                //memberRepository.delete(memberEntity);
+                memberRepository.deleteById(id);
+                return 1;
+            }
+            else{
+                return 0;
+            }
+
+        }
+        else{
+            return 0;
+        }
+    }
+}
