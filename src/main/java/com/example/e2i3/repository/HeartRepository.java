896a3585e463ab3a8246ebdb1f4d8c1dea70fb2b
최재원 <46291115+choijaewon323@users.noreply.github.com>
2023-04-27 20:35:55 +0900

    테스트1

--- src/main/java/com/example/e2i3/repository/HeartRepository.java
+++ src/main/java/com/example/e2i3/repository/HeartRepository.java
@@ -1,14 +1,12 @@
 package com.example.e2i3.repository;
 
 import com.example.e2i3.entity.Board;
-import com.example.e2i3.entity.Like;
+import com.example.e2i3.entity.Heart;
 import com.example.e2i3.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface LikeRepository extends JpaRepository<Like, Long> {
-    Optional<Like> findByBoardAndMember(Board board, Member member);
-
-
+public interface HeartRepository extends JpaRepository<Heart, Long> {
+    Optional<Heart> findByBoardAndMember(Board board, Member member);
 }
